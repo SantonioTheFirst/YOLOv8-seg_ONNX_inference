8d1d5451b458e7664657819a4ee54b4d93d98712
SantonioTheFirst <44162170+SantonioTheFirst@users.noreply.github.com>
2023-11-29 06:47:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -42,6 +42,7 @@ def process_output_masks(image, masks):
         #cropped = (np.stack((mask, ) * 3, axis=-1) * image)
         #mask = cv2.erode(mask, kernel=(3, 3), iterations=2)
         #mask = (mask * 255.0).astype(np.uint8)
+        st.info(f'{mask.max()}, {mask.shape}')
         contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         contour = sorted(contours, key=cv2.contourArea, reverse=True)[0]
         black = np.zeros_like(mask)
